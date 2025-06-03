2752afe9128bb63b75da890369be410fc7d35fb6
alexander <alexander.t.simonsen@gmail.com>
2025-06-03 07:57:26 +0200

    bildPush2

--- src/data/courses.jsx
+++ src/data/courses.jsx
@@ -4,19 +4,19 @@ const courses = [
     id: 1,
     title: "Webbutveckling",
     description: "Lär dig HTML, CSS, JavaScript och moderna ramverk.",
-    image: "https://picsum.photos/id/1011/1200/500",
+    image: "https://plus.unsplash.com/premium_photo-1661414473396-4600573d1f33?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
     title: "Datavetenskap",
     description: "Algoritmer, datastrukturer och programmering i djupet.",
-    image: "https://picsum.photos/id/1005/1200/500",
+    image: "https://cdn.pixabay.com/photo/2024/05/21/19/57/computer-8779040_1280.jpg",
   },
   {
     id: 3,
     title: "UI/UX Design",
     description: "Designa användarupplevelser med stil och funktion.",
-    image: "https://picsum.photos/id/1025/1200/500",
+    image: "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
